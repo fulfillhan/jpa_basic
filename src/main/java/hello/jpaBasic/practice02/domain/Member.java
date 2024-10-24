a4db0fd1e969f36b3d99ce8f20841322e9f0e9fc
sanghui <tkdgml719@gamil.com>
2024-10-24 16:57:30 +0900

    값 타입 -기본값과 임베디드 타입

---
+++ src/main/java/hello/jpaBasic/practice02/domain/Member.java
@@ -0,0 +1,49 @@
+package hello.jpaBasic.practice02.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    @Column(name = "USERNAME")
+    private String username;
+    @Embedded  // 값 타입을 사용하는 곳에 표시
+    private Period period;
+    @Embedded
+   private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Period getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Period period) {
+        this.period = period;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
