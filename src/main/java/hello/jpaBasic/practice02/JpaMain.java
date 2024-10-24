ef3488e2dc72dc1591488a68432fed5efe9890f6
sanghui <tkdgml719@gamil.com>
2024-10-24 23:53:57 +0900

    값 타입 - 값 타입 컬렉션과 적용

--- src/main/java/hello/jpaBasic/practice02/JpaMain.java
+++ src/main/java/hello/jpaBasic/practice02/JpaMain.java
@@ -7,6 +7,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+import java.util.List;
+import java.util.Set;
+
 /*임베디드 타입과 값 타입에 대해서*/
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,15 +21,45 @@ public class JpaMain {
 
         try {
 
-            Address address = new Address("city","street","zipcode");
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setAddress(address);
+         Member member = new Member();
+         member.setUsername("member1");
+         member.setAddress(new Address("city","street","zipcode"));
+
+         //컬렉션
+            member.getFavoriteFood().add("치킨");
+            member.getFavoriteFood().add("떡볶이");
+            member.getFavoriteFood().add("피자");
+
+            member.getAddressHistory().add(new Address("old1","street","zipcode"));
+            member.getAddressHistory().add(new Address("old2","street","zipcode"));
+
             em.persist(member);
 
             //영속성 정리
             em.flush();
             em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            //컬렉션도 지연 로딩이다.
+            List<Address> addressHistory = findMember.getAddressHistory();
+            for (Address address : addressHistory) {
+                System.out.println("address.getCity() = " + address.getCity());
+            }
+
+            Address findAddress = findMember.getAddress();
+            //불변객체 - 새로 만들어야함
+            findMember.setAddress(new Address("newCity",findAddress.getStreet(),findAddress.getZipCode()));
+
+            //컬렉션의 값 변경
+            //값 타입은 멤버에 의존관계를 맡긴다.
+            findMember.getFavoriteFood().remove("치킨");
+            findMember.getFavoriteFood().add("쌀국수");
+
+            //객체 전체를 변경-> Address객체는 equals와 hashCode 재정의 되어 있어야 한다.
+          /*  findMember.getAddressHistory().remove(new Address("newCity",findAddress.getStreet(),findAddress.getZipCode());
+            findMember.getAddressHistory().add(new Address("newCity",findAddress.getStreet(),findAddress.getZipCode());*/
+
 
             tx.commit();
         }catch (Exception e){
--- src/main/java/hello/jpaBasic/practice02/domain/Member.java
+++ src/main/java/hello/jpaBasic/practice02/domain/Member.java
@@ -2,6 +2,11 @@ package hello.jpaBasic.practice02.domain;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 public class Member {
     @Id
@@ -12,8 +17,18 @@ public class Member {
     private String username;
     @Embedded  // 값 타입을 사용하는 곳에 표시
     private Period period;
-    @Embedded
+    @Embedded //값타입
    private Address address;
+
+    //값 타입 컬렉션
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD")
+    @JoinColumn(name = "MEMBER_ID")//어떤 컬럼과 조인해야 하는지
+    @Column(name = "FOOD_NAME")//컬럼명을 바꿀 수 있다.
+    private Set<String> favoriteFood = new HashSet<>();
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =@JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -45,5 +60,21 @@ public class Member {
 
     public void setAddress(Address address) {
         this.address = address;
+    }
+
+    public Set<String> getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(Set<String> favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
