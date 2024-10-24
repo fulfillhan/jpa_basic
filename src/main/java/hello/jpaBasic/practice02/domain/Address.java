e9da2de078de8d7538a62d172e96ab854c66b06a
sanghui <tkdgml719@gamil.com>
2024-10-24 17:44:54 +0900

    값 타입 -값 타입의 비교 -Address수정

--- src/main/java/hello/jpaBasic/practice02/domain/Address.java
+++ src/main/java/hello/jpaBasic/practice02/domain/Address.java
@@ -2,6 +2,8 @@ package hello.jpaBasic.practice02.domain;
 
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
 public class Address {
     private String city;
@@ -9,30 +11,37 @@ public class Address {
     private String zipCode;
 
     public Address(String city, String street, String zipCode) {
+        this.city = city;
+        this.street = street;
+        this.zipCode = zipCode;
     }
 
+    public Address() {
+    }
+    // 객체 타입은 immutable 해야 한다.  set타입 제거
     public String getCity() {
         return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public String getStreet() {
         return street;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
     public String getZipCode() {
         return zipCode;
     }
 
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
+    //equals , hashCode 재정의
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipCode, address.zipCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipCode);
     }
 }
-
